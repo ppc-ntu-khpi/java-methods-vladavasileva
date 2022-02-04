a00641d358b4db5d5a9ec241b590b558ec128b6f
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2022-02-04 20:32:46 +0200

    Add files via upload

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,24 @@
 //package domain;
-
+/**
+ * Гловний клас, який обраховує суму всіх чисел, та зводить її до однієї цифри. 
+ * @author Влада
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static String Calculate(String date){
+        while(date.length()>1){/**
+         * цикл який вираховує довжину рядку
+         */
+            int sum = 0;
+            /**
+             * цикл який перетворює символ в число та рахує суму 
+             */
+            for(char c: date.toCharArray()){
+                if(Character.isDigit(c)){
+                    sum+=(c-'0');
+                }
+             date = String.valueOf(sum);
+} 
+        }
+        return date;
     }
 }
